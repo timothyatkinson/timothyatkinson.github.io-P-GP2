97757d3104cca94e5df7fbc044b547125c1681b2
timothyatkinson <tjatkinson95@gmail.com>
2018-06-07 20:24:03 +0100

    Dont need quote fix now

--- gp2converter.js
+++ gp2converter.js
@@ -22,8 +22,6 @@ function convert_graph(data){
 		id = node.split(",")[0];
 		label = node.split(",")[1];
 		label = label.trim();
-		var re = new RegExp("\"", 'g');
-		label = label.replace(re, "\\\"");
 		id = id.trim();
 		console.log(id);
 		console.log(label);
