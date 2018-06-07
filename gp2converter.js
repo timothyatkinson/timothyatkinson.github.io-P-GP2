58d5e726f35424e83fab443b9779bca62683a6b1
timothyatkinson <tjatkinson95@gmail.com>
2018-06-07 18:25:42 +0100

    test4

--- gp2converter.js
+++ gp2converter.js
@@ -4,6 +4,17 @@ function convert_graph(data){
 	nodes = nodes.split("[")[1];
 	edges = edges.split("]")[0];
 	edges = edges.replace("]", "");
+	
+	var digraph = "digraph G {";	
+	var nodeList = nodes.split(")");
+	var i;
+	for(i = 0; i < nodeList.length; i++){
+		node = nodeList[i];
+		node = node.split("(")[1];
+		id = node.split(",")[0];
+		id = id.trim();
+		console.log(id);
+	}
 	console.log(nodes);
 	console.log(edges);
 	console.log("Data: " + data);
