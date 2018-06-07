59d6b541d03cad11d63ffc056589c18bb0a9a572
timothyatkinson <tjatkinson95@gmail.com>
2018-06-07 18:16:54 +0100

    test2

--- gp2converter.js
+++ gp2converter.js
@@ -1,9 +1,10 @@
 function convert_graph(data){
 	nodes = data.split("|")[0];
 	edges = data.split("|")[1];
-	nodes.replace("[", "");
-	edges.replace("]", "");
+	nodes = nodes.replace("[", "");
+	edges = edges.replace("]", "");
 	console.log(nodes);
 	console.log(edges);
+	console.log("Data: " + data);
 	return "digraph G { a1 -> b3 }"
 }
