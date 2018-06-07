1412d90fab063b64b1f41372bd71bcdec1bfa492
timothyatkinson <tjatkinson95@gmail.com>
2018-06-07 20:03:50 +0100

    asdf

--- gp2converter.js
+++ gp2converter.js
@@ -22,6 +22,8 @@ function convert_graph(data){
 		id = node.split(",")[0];
 		label = node.split(",")[1];
 		label = label.trim();
+		var re = new RegExp("\"", 'g');
+		label = label.replace(re, "\\");
 		id = id.trim();
 		console.log(id);
 		console.log(label);
