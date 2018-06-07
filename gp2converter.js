949e9d967035e58b0ea7a4dc264c6bae17125382
timothyatkinson <tjatkinson95@gmail.com>
2018-06-07 18:09:00 +0100

    Dummy converter

---
+++ gp2converter.js
@@ -0,0 +1,3 @@
+function convert_graph(data){
+	return "digraph G { a1 -> b3 }"
+}
--- worker.js
+++ worker.js
@@ -1,7 +1,8 @@
 importScripts("bower_components/viz.js/viz.js");
+importScripts("gp2converter.js");
 
 onmessage = function(e) {
 	console.log(e.data.src);
-  var result = Viz(e.data.src, e.data.options);
+  var result = Viz(convert_graph(e.data.src), e.data.options);
   postMessage(result);
 }
