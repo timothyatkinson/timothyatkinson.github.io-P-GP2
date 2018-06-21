0da30210127005b4840432247acfacad814fb2d8
Tim Atkinson <tja511@york.ac.uk>
2018-06-21 10:46:05 +0100

    Begin rule visualiser

--- worker.js
+++ worker.js
@@ -6,3 +6,8 @@ onmessage = function(e) {
   var result = Viz(convert_graph(e.data.src), e.data.options);
   postMessage(result);
 }
+onmessagerule = function(e) {
+	console.log(e.data.src);
+  var result = Viz(convert_graph(e.data.src), e.data.options);
+  postMessage(result);
+}
