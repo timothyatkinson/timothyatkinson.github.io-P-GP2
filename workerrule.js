c83d2d9e4bc6bec60f594f6da85bcae920fed4d5
Tim Atkinson <tja511@york.ac.uk>
2018-06-21 10:55:50 +0100

    Onmessage<=onmessagerule

--- workerrule.js
+++ workerrule.js
@@ -1,6 +1,7 @@
 importScripts("bower_components/viz.js/viz.js");
 importScripts("gp2converter.js");
-onmessagerule = function(e) {
+
+onmessage = function(e) {
 	console.log(e.data.src);
   var result = Viz(convert_rule(e.data.src), e.data.options);
   postMessage(result);
